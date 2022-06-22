d33572f7323f65b41849fdda4b14a47856b175e1
Prajanya Singh <89376370+prajanya06@users.noreply.github.com>
2022-06-22 20:40:52 +0530

    Add files via upload

---
+++ Assignment1/Ques3.cpp
@@ -0,0 +1,57 @@
+//Written in C++17
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n, i=0;
+	cout<<"Welcome to Stacks!"<<endl;
+	cin>>n;
+	stack<int> st;
+	string str="star";
+	while(str!="stop"){
+	    cout<<"Give one of options: pop, push, stop"<<endl;
+	    cin>>str;
+	    if(str=="push"){
+	        if(i==n){
+	            cout<<"Can't push anymore"<<endl;
+	            break;
+	        }
+	        int k=0;
+	        cin>>k;
+	        st.push(k);
+	        i++;
+	    }
+	    else if(str=="pop"){
+	        int k = st.top();
+	        cout<<"Element Poped : "<<k<<endl;
+	        st.pop();
+	        i--;
+	    }
+	}
+	stack<int> stk;
+	while(!st.empty()){
+	    int k = st.top();
+	    st.pop();
+	    while(!stk.empty() && stk.top()>k){
+	        st.push(stk.top());
+	        stk.pop();
+	    }
+	    stk.push(k);
+	}
+	while(!stk.empty()){
+	    int k=stk.top();
+	    st.push(k);
+	    stk.pop();
+	}
+	cout<<"Stack elemnts are :"<<endl;
+	while(!st.empty()){
+	    int k=st.top();
+	    cout<<"|"<<k<<"|"<<endl;
+	    cout<<"---"<<endl;
+	    st.pop();
+	}
+	return 0;
+}
